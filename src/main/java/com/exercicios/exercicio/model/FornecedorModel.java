9b6174018e7103e6ab7f6bad4ef7befd5463018b
gmiashiro <g.miashiro.freitas@gmail.com>
2026-03-16 19:52:35 -0300

    feat: setup inicial do projeto e camada model

---
+++ src/main/java/com/exercicios/exercicio/model/FornecedorModel.java
@@ -0,0 +1,52 @@
+package com.exercicios.exercicio.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class FornecedorModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "fantasiaNome")
+    private String nomeFantasia;
+    @Column(name = "cnpjcpf")
+    private String cnpj;
+    private String contato;
+
+    public FornecedorModel(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+}
